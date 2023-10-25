36ef62c2cd9999e8d7e4e8a17d8c9ac8063c5d26
Sinisa <sinisa_neskovic@yahoo.com>
2023-10-26 01:11:42 +0200

    add Solution-02-JPA project

---
+++ Labs/Solution-02-JPA/src/main/java/dk/lundogbendsen/recipes/model/Category.java
@@ -0,0 +1,5 @@
+package dk.lundogbendsen.recipes.model;
+
+public enum Category {
+	BREAKFAST, LUNCH, SNACK, STARTER, MAIN_COURSE, DESERT;
+}
---
+++ Labs/Solution-02-JPA/src/main/java/dk/lundogbendsen/recipes/model/Ingredient.java
@@ -0,0 +1,53 @@
+package dk.lundogbendsen.recipes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Ingredient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Ingredient_SEQ")
+	@SequenceGenerator(name = "Ingredient_SEQ", allocationSize = 1)
+	private Long id;
+	@Column(unique = true, length = 60)
+	private String name;
+	private MeasureUnit unit;
+	private double calories;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public MeasureUnit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(MeasureUnit unit) {
+		this.unit = unit;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public double getCalories() {
+		return calories;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", name=" + name + ", unit=" + unit
+				+ ", calories=" + calories + "]";
+	}
+
+	
+	
+	
+}
