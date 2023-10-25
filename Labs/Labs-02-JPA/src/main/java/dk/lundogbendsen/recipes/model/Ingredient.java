6fed17de6c428eb6570b33048495ac129ba8612b
Sinisa <sinisa_neskovic@yahoo.com>
2023-10-26 01:23:10 +0200

    remove annotation for Ingredient_SEQ and correct artifactId in Labs-02-JPA

--- Labs/Labs-02-JPA/src/main/java/dk/lundogbendsen/recipes/model/Ingredient.java
+++ Labs/Labs-02-JPA/src/main/java/dk/lundogbendsen/recipes/model/Ingredient.java
@@ -5,8 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Ingredient {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Ingredient_SEQ")
-	@SequenceGenerator(name = "Ingredient_SEQ", allocationSize = 1)
+	@GeneratedValue
 	private Long id;
 	@Column(unique = true, length = 60)
 	private String name;
