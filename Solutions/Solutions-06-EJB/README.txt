1dcd7664e5b91b3b2ef7c94854e4106a18d1e779
jakob bendsen <jakob@lundogbendsen.dk>
2023-11-04 10:38:28 +0100

    added project

---
+++ Solutions/Solutions-06-EJB/README.txt
@@ -0,0 +1,9 @@
+Solution for Labs-06-EJB
+
+How to run:
+mvn install -P mvn-deploy
+
+Watch the console output. Tables should be created and data inserted.
+
+Also see
+http://localhost:8080/Solutions-06-EJB-1.0-SNAPSHOT/GatewayServlet
---
+++ Solutions/Solutions-06-EJB/src/main/java/com/recipes/web/RecipeServlet.java
@@ -0,0 +1,56 @@
+package com.recipes.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.Set;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.recipes.ejb.RecipeDao;
+import com.recipes.ejb.RecipeService;
+import com.recipes.model.Recipe;
+
+@WebServlet("/RecipeServlet")
+public class RecipeServlet extends HttpServlet {
+	@EJB
+	RecipeDao dao;
+	
+	@EJB
+	RecipeService service;
+	
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		Set<Recipe> all = dao.findAll();
+		DecimalFormat f = new DecimalFormat("#,###,###,##0.00");
+		
+		out.print("<html>");
+		out.print("<body>");
+		out.print("<h1>Calories table</h1>");
+		out.print("<table border='1'>");
+		for (Recipe recipe : all) {
+			out.print("<th>Recipe</th>");
+			out.print("<th>Calories</th>");
+
+			out.print("<tr>");
+				out.print("<td>");
+					out.print(recipe.getName());
+				out.print("</td>");
+				out.print("<td>");
+					out.print(f.format(service.calculateCalories(recipe)));
+				out.print("</td>");			
+			out.print("</tr>");
+		}
+		out.print("</table>");		
+		out.print("</body>");		
+		out.print("</html>");
+	}
+}
