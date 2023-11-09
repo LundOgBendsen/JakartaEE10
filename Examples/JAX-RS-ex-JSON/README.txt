76cb6d97a4fd559fd4add2383c10948371ca75f8
jakob bendsen <jakob@lundogbendsen.dk>
2023-11-09 13:06:50 +0100

    added ex with jsonb + added text in hello world readme.txt

--- Examples/JAX-RS-ex-JSON/README.txt
+++ Examples/JAX-RS-ex-JSON/README.txt
@@ -6,6 +6,12 @@ Run:
 mvn install -P mvn-deploy
 
 See:
+Made by JSONB
+http://localhost:8080/JAX-RS-ex-JSON-1.0-SNAPSHOT/resources/products/1
+
+Made by JSON Generator
 http://localhost:8080/JAX-RS-ex-JSON-1.0-SNAPSHOT/resources/products/2
+
+Made by JSON Object
 http://localhost:8080/JAX-RS-ex-JSON-1.0-SNAPSHOT/resources/products/3
 
--- Examples/JAX-RS-ex-JSON/src/main/java/dk/lundogbendsen/rest/ProductResource.java
+++ Examples/JAX-RS-ex-JSON/src/main/java/dk/lundogbendsen/rest/ProductResource.java
@@ -1,5 +1,6 @@
 package dk.lundogbendsen.rest;
 
+import dk.lundogbendsen.rest.model.Product;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.json.stream.JsonGenerator;
@@ -17,30 +18,15 @@ import java.io.*;
 public class ProductResource {
 
     @GET
-    @Path("/3")
+    @Path("/1")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProduct3() {
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
-
-        final JsonObject model = Json.createObjectBuilder()
-                .add("id", 3)
-                .add("name", "Keyboard")
-                .add("price", 180.0)
-                .build();
-
-        StreamingOutput stream = new StreamingOutput() {
-            @Override
-            public void write(OutputStream os) throws IOException,
-                    WebApplicationException {
-                Writer writer = new BufferedWriter(new OutputStreamWriter(os));
-                writer.write(model.toString());
-                writer.flush();
-            }
-        };
-        return Response.ok(stream).type(MediaType.APPLICATION_JSON).build();
+    public Response getProduct1() {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("keyboard");
+        product.setPrice(100.0);
+        return Response.ok(product).type(MediaType.APPLICATION_JSON).build();
     }
-
 
     @GET
     @Path("/2")
@@ -66,6 +52,31 @@ public class ProductResource {
                     throw new RuntimeException(e);
                 }
 
+            }
+        };
+        return Response.ok(stream).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/3")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProduct3() {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
+
+        final JsonObject model = Json.createObjectBuilder()
+                .add("id", 3)
+                .add("name", "Keyboard")
+                .add("price", 180.0)
+                .build();
+
+        StreamingOutput stream = new StreamingOutput() {
+            @Override
+            public void write(OutputStream os) throws IOException,
+                    WebApplicationException {
+                Writer writer = new BufferedWriter(new OutputStreamWriter(os));
+                writer.write(model.toString());
+                writer.flush();
             }
         };
         return Response.ok(stream).type(MediaType.APPLICATION_JSON).build();
