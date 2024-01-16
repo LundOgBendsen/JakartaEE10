a0fa55a17a7b11a65a92c4e8b9a607829d50d2ac
Jakob Bendsen <jakob@lundogbendsen.dk>
2024-01-16 16:46:23 +0100

    added project

---
+++ Solutions/Solutions-03-BeanValidation/README.txt
@@ -0,0 +1,6 @@
+Solution for Lab-03-BeanValidation
+
+Deploy the application and watch the console output. Tables should be 
+created and data inserted. 
+
+ 
---
+++ Solutions/Solutions-03-BeanValidation/src/main/java/com/recipes/model/RecipeIngredient.java
@@ -0,0 +1,56 @@
+package com.recipes.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class RecipeIngredient {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private double quantity;
+	@OneToOne(cascade={CascadeType.PERSIST})
+	private Ingredient ingredient;
+	@ManyToOne
+	private Recipe recipe;
+	
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	@Override
+	public String toString() {
+		return "RecipeIngredient [id=" + id + ", quantity=" + quantity + "]";
+	}
+
+}
