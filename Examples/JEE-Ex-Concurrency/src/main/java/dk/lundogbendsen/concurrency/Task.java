7dae850af830d18952bec828d246245579dfa208
Sinisa <sinisa_neskovic@yahoo.com>
2024-02-04 01:27:27 +0100

    add abstract class Task in JEE-Ex-Concurrency

--- Examples/JEE-Ex-Concurrency/src/main/java/dk/lundogbendsen/concurrency/Task.java
+++ Examples/JEE-Ex-Concurrency/src/main/java/dk/lundogbendsen/concurrency/Task.java
@@ -16,16 +16,12 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.logging.Logger;
 
 /* The tasks just notify the JAX-RS web service in the EJB */
-public class Task implements Runnable {
-
-    private static final Logger log = Logger.getLogger("Task");
+public abstract class Task implements Runnable {
     private static final String WS_URL =
             "http://localhost:8080/taskcreator/jaxrs/taskinfo";
 
@@ -33,41 +29,15 @@ public class Task implements Runnable {
     private final String type;
     private final DateFormat dateFormat;
     private final Client client;
-    private int counter;
 
     public Task(String n, String t) {
         name = n;
         type = t;
-        counter = 1;
         dateFormat = new SimpleDateFormat("HH:mm:ss");
         client = ClientBuilder.newClient();
-        /* For delayed tasks, send
-        /* Send: 14:15:45 - DELAYED Task ABCDE submitted */
-        if (type.compareTo("DELAYED") == 0)
-            sendToWebService("submitted");
     }
-    
-    @Override
-    public void run() {
-        /* Send: 14:15:45 - TASKTYPE Task ABCDE started */
-        if (type.compareTo("PERIODIC") == 0)
-            sendToWebService("started run #" + counter);
-        else
-            sendToWebService("started");
-        
-        try {
-            Thread.sleep(1500);
-        } catch (Exception e) { }
-        
-        /* Send: 14:15:47 - TASKTYPE Task ABCDE finished */
-        if (type.compareTo("PERIODIC") == 0)
-            sendToWebService("finished run #" + (counter++));
-        else
-            sendToWebService("finished");
-    }
-    
-    /* Send: 14:15:47 - TASKTYPE Task ABCDE [details] */
-    protected void sendToWebService(String details) {
+
+      protected void sendToWebService(String details) {
         String time = dateFormat.format(Calendar.getInstance().getTime());
         String msg = time + " - "  + type + " Task " + name + " " + details;
         Response resp = client.target(WS_URL)
