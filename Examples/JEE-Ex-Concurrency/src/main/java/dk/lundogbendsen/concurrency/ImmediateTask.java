6ea49ac3858d6f0669e03c4e840b2784f3d616fa
Sinisa <sinisa_neskovic@yahoo.com>
2024-02-07 23:24:26 +0100

    fix immediate task to send started message (in JEE-Ex-Concurrency)

--- Examples/JEE-Ex-Concurrency/src/main/java/dk/lundogbendsen/concurrency/ImmediateTask.java
+++ Examples/JEE-Ex-Concurrency/src/main/java/dk/lundogbendsen/concurrency/ImmediateTask.java
@@ -7,11 +7,12 @@ public class ImmediateTask extends Task {
 
     public ImmediateTask(String n) {
         super(n, "IMMEDIATE");
-        sendToWebService("started");
+
     }
 
     @Override
     public void run() {
+        sendToWebService("started");
         try {
             Thread.sleep(1500);
         } catch (Exception e) {
